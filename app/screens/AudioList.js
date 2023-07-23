8dd409a109f23736be3855307a2e8c070cd3f8cc
FabianPonczko <fabianponczko@live.com.ar>
2023-07-23 19:50:25 -0300

    init

---
+++ app/screens/AudioList.js
@@ -0,0 +1,24 @@
+import { Text, View ,StyleSheet,ScrollView} from 'react-native'
+import React, { Component } from 'react'
+import { AudioContext } from '../context/AudioProvider'
+
+
+export class AudioList extends Component {
+    static contextType = AudioContext
+  render() {
+    return (
+      <ScrollView >
+        {this.context.audioFiles.map(item=><Text style={{borderBottomColor:"grey",borderWidth:1,padding:10}} key={item.id}>{item.filename}</Text> )}
+      </ScrollView>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:"center",
+        alignItems:"center",
+    }
+})
+export default AudioList
